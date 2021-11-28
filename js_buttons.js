49a4212917afef0e9c8eed8243670a61e46536eb
Sylvie Bielak <sbielak2021@jpkeefehs.org>
2021-11-28 16:40:30 -0500

    Uploaded stuff for final SWEN 101 project

---
+++ js_buttons.js
@@ -0,0 +1,11 @@
+function send_to_page(id){
+    window.location.href = id;
+}
+
+
+function show (id){
+    document.getElementById(id).style.display = "block";
+}
+function hide (id){
+    document.getElementById(id).style.display = "none";
+}
